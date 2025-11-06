ccf6bee3103c0e68ad24a30bb3d3f176c2903382
jarv1s <19623185@std.stei.itb.ac.id>
2025-11-06 22:02:20 +0700

    v1.3 - conditional radar lock

--- Anjing.py
+++ Anjing.py
@@ -8,12 +8,14 @@ from robocode_tank_royale.bot_api.events import ScannedBotEvent
 # ------------------------------------------------------------------
 """
 ===== RADAR =====
-V1.0
+v1.0
 - kosongan
-V1.1
+v1.1
 - radar 360
-V1.2
+v1.2
 - radar lock
+v1.3
+- conditional radar lock
 
 """
 # ------------------------------------------------------------------
@@ -23,8 +25,9 @@ class Anjing(Bot):
         self.set_turn_radar_left(float('inf'))
 
     async def on_scanned_bot(self, scanned_bot_event: ScannedBotEvent) -> None:
-        sudut = self.normalize_relative_angle(self.radar_bearing_to(scanned_bot_event.x, scanned_bot_event.y))
-        self.set_turn_radar_left(float('inf') * sudut)
+        if (scanned_bot_event.energy < 100):
+            sudut = self.normalize_relative_angle(self.radar_bearing_to(scanned_bot_event.x, scanned_bot_event.y))
+            self.set_turn_radar_left(float('inf') * sudut)
 
 async def main() -> None:
     bot = Anjing()
