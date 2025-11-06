82ad51bf821b03d649bc98005edc9b9482f14af0
jarv1s <19623185@std.stei.itb.ac.id>
2025-11-07 02:31:08 +0700

    refactor: move version tracker to readme

--- Anjing.py
+++ Anjing.py
@@ -7,31 +7,6 @@ import math
 # ------------------------------------------------------------------
 # Anjing
 # ------------------------------------------------------------------
-"""
-===== RADAR =====
-v1.0
-- kosongan
-v1.1
-- radar 360
-v1.2
-- radar lock
-v1.3
-- conditional radar lock
-
-===== FIRE =====
-v2.0
-- fire
-v2.1
-- fire ke scanned bot / head-on targeting
-v2.2.1
-- precise linear targeting
-v2.2.2
-- approx linear targeting (singkat)
-v2.2.3
-- explanation of approx linear targeting
-
-"""
-# ------------------------------------------------------------------
 
 class Anjing(Bot):
     async def run(self) -> None:
