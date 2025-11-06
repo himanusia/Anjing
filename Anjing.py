64c13a46600926ba2b52c09cfc14f7b5ea585d2d
jarv1s <19623185@std.stei.itb.ac.id>
2025-11-06 21:58:55 +0700

    v1.2 - radar lock

--- Anjing.py
+++ Anjing.py
@@ -1,21 +1,30 @@
 import asyncio
 
 from robocode_tank_royale.bot_api.bot import Bot
+from robocode_tank_royale.bot_api.events import ScannedBotEvent
 
 # ------------------------------------------------------------------
 # Anjing
 # ------------------------------------------------------------------
 """
+===== RADAR =====
 V1.0
 - kosongan
 V1.1
 - radar 360
+V1.2
+- radar lock
+
 """
 # ------------------------------------------------------------------
+
 class Anjing(Bot):
     async def run(self) -> None:
         self.set_turn_radar_left(float('inf'))
 
+    async def on_scanned_bot(self, scanned_bot_event: ScannedBotEvent) -> None:
+        sudut = self.normalize_relative_angle(self.radar_bearing_to(scanned_bot_event.x, scanned_bot_event.y))
+        self.set_turn_radar_left(float('inf') * sudut)
 
 async def main() -> None:
     bot = Anjing()
